a4a40ee6383b4d5fc6538e80ced0f0af9aadc433
Haesol <haesol87@gmail.com>
2016-05-14 14:51:13 -0400

    Fixed minor stuff from kraving model.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var methodOverride = require('method-override');
 
 var Kraving = require('./models/kraving');
 
@@ -26,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'));
 
 app.use('/', routes);
 app.use('/users', users);
--- models/kraving.js
+++ models/kraving.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 
 var KravingSchema = new mongoose.Schema({
   name:  {type: String, required: true },
-  city:  type: String,
-  state: type: String,
-  zip:  type: Integer,
-  favorite:  type: Boolean
-}, timestamps: true});
+  city:  String,
+  state: String,
+  zip:   Number,
+  favorite:  Boolean
+}, {timestamps: true});
 
 module.exports = mongoose.model('Kraving', KravingSchema);
