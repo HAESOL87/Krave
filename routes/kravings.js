aca2a1795297797b28a356377cd393ecaf75e365
Haesol <haesol87@gmail.com>
2016-05-18 17:47:12 -0400

    Fav authentication works.

--- routes/kravings.js
+++ routes/kravings.js
@@ -19,26 +19,11 @@ function makeError(res, message, status) {
   return error;
 }
 
-// // INDEX
-// router.get('/', authenticate, function(req, res, next) {
-//   var todos = global.currentUser.todos;
-//   res.render('todos/index', { todos: todos, message: req.flash() });
-// });
-
 // Index - get all kravings and render kravings list view
 router.get('/', authenticate, function(req, res, next) {
   var kravings = global.currentUser.kravings;
   res.render('kravings/index', { kravings: kravings, message: req.flash() } );
 });
-
-
-//   Kraving.find({})
-//   .then(function(kravings) {
-//     res.render('kravings/index', { kravings: kravings, message: req.flash() } );
-//   }, function(err) {
-//     return next(err);
-//   });
-// });
 
 // New Kraving
 router.get('/new', authenticate, function(req, res, next) {
@@ -58,37 +43,12 @@ router.get('/:id', authenticate, function(req, res, next) {
     res.render('kravings/showGlobalMap', { kraving: kraving, message: req.flash()});
 });
 
-
-// // Show Global Map
-// router.get('/:id', function(req, res, next) {
-//   Kraving.findById(req.params.id)
-//   .then(function(kraving) {
-//     if (!kraving) return next(makeError(res, 'Document not found', 404));
-//     res.render('kravings/showGlobalMap', { kraving: kraving});
-//   }, function(err) {
-//     return next(err);
-//   });
-// });
-
 // Show Info
 router.get('/:kraving/info/:place', authenticate, function(req, res, next) {
   var kraving = currentUser.kravings.id(req.params.kraving);
   if (!kraving) return next(makeError(res, 'Document not found', 404));
     res.render('kravings/showInfo', { kraving: kraving, currentPlaceId: req.params.place, message: req.flash()});
 });
-
-// // Show Info
-// router.get('/:kraving/info/:place', function(req, res, next) {
-//   console.log("I'm here!");
-//   console.log('req.params:', req.params);
-//   Kraving.findById(req.params.kraving)
-//   .then(function(kraving) {
-//     if (!kraving) return next(makeError(res, 'Document not found', 404));
-//     res.render('kravings/showInfo', { kraving: kraving, currentPlaceId: req.params.place});
-//   }, function(err) {
-//     return next(err);
-//   });
-// });
 
 // Create Kraving
 router.post('/', authenticate, function(req, res, next) {
@@ -107,39 +67,12 @@ router.post('/', authenticate, function(req, res, next) {
   });
 });
 
-// // Create Kraving
-// router.post('/', function(req, res, next) {
-//   var kraving = new Kraving({
-//     name: req.body.name,
-//     city: req.body.city,
-//     state: req.body.state,
-//     zip: req.body.zip
-//   });
-//   kraving.save()
-//   .then(function(saved) {
-//     res.redirect('/kravings');
-//   }, function(err) {
-//     return next(err);
-//   });
-// });
-
 // Edit Kraving
 router.get('/:id/edit', authenticate, function(req, res, next) {
     var kraving = currentUser.kravings.id(req.params.id);
     if (!kraving) return next(makeError(res, 'Document not found', 404));
     res.render('kravings/edit', { kraving: kraving, message: req.flash() });
 });
-
-// // Edit Kraving
-// router.get('/:id/edit', function(req, res, next) {
-//   Kraving.findById(req.params.id)
-//   .then(function(kraving) {
-//     if (!kraving) return next(makeError(res, 'Document not found', 404));
-//     res.render('kravings/edit', { kraving: kraving });
-//   }, function(err) {
-//     return next(err);
-//   });
-// });
 
 // Update Kraving
 router.put('/:id', authenticate, function(req, res, next) {
@@ -159,24 +92,6 @@ router.put('/:id', authenticate, function(req, res, next) {
 }
 });
 
-// // Update Kraving
-// router.put('/:id', function(req, res, next) {
-//   Kraving.findById(req.params.id)
-//   .then(function(kraving) {
-//     if (!kraving) return(makeError(res, 'Document not found', 404));
-//     kraving.name = req.body.name;
-//     kraving.city = req.body.city;
-//     kraving.state = req.body.state;
-//     kraving.zip = req.body.zip;
-//     return kraving.save();
-//   })
-//   .then(function(saved) {
-//     res.redirect('/kravings');
-//   }, function(err) {
-//     return next(err);
-//   });
-// });
-
 // Destroy Kraving
 router.delete('/:id', authenticate, function(req, res, next) {
   var kraving = currentUser.kravings.id(req.params.id);
@@ -191,18 +106,5 @@ router.delete('/:id', authenticate, function(req, res, next) {
     return next(err);
   });
 });
-
-
-
-// // Destroy Kraving
-// router.delete('/:id', function(req, res, next) {
-//   Kraving.findByIdAndRemove(req.params.id)
-//   .then(function() {
-//     res.redirect('/kravings');
-//   }, function(err) {
-//     return next(err);
-//   });
-// });
-
 
 module.exports = router;
